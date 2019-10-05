b24ef80280292aec92dce86c66ab863298b6e58b
Thomas Andrew <thomas@enotea.uk>
2019-10-05 12:35:10 +0100

    add test cask

---
+++ Casks/tomjonandy-agenda.rb
@@ -0,0 +1,14 @@
+cask 'tomjonandy-agenda' do
+  version '7.0'
+  sha256 '7fe5fe695188ea10ec2042155423e12809cf6ede5d3babc204aeab14a93f3bfb'
+
+  url "https://downloads.agenda.com/Agenda_#{version}.zip"
+  appcast 'https://downloads.agenda.com/AgendaSparkleAppcast.xml'
+  name 'Agenda'
+  homepage 'https://agenda.com/'
+
+  depends_on macos: '>= :sierra'
+
+  app 'Agenda.app'
+end
+
